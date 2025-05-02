db22b03b71d97aad2ca9787baddda03576d36620
vikasthakurr <vikaskumar20012001@gmail.com>
2025-05-02 12:22:58 +0530

    letvarconst

---
+++ javascript/let vs var vs const.js
@@ -0,0 +1,61 @@
+// var name = "Vikas Thakur";
+// let age = 25;
+// const city = "Delhi";
+// salary = 122334455;
+
+// var  case
+
+// var name = "abcd";
+// name ="akash"
+// console.log(name);
+
+// let case
+
+// let age = 30;
+// age =36;
+// console.log(age);
+
+// const city = "jaipur";
+// city = "jaipur";
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(salary);
+
+// console.table({name,age,city,salary});
+// console.log(a);
+// var a = 10;
+
+// const b = 20;
+// console.log(b);
+
+// const c=30;
+// console.log(c);
+
+console.log(d);
+var a = 19;
+let b = 30;
+console.log(c);
+const c = 40;
+function outside(){
+    var a=30;
+    let b=40;
+    console.log(a);
+    console.log(b);
+
+    function inside(){
+        var a=50;
+        let b=60;
+        console.log(a);
+        console.log(b);
+    }
+    inside();
+}
+outside();
+var d = "vikas";
+console.log(d);
+console.log(a);
+console.log(b);
+console.log(c);
+
---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// var a = 24;
+// var b = 30;
+// console.log(a);
+
+
+
+// console.log(a);
+const a=10;
+console.log(a);
+
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+//   console.log("inside function");
+// }
+// var res = sum(5, 10);
+// console.log(res);
+// var res1 = sum(a, b);
+// console.log(res1);
