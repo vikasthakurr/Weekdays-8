0f19bffe9326867322cf17ec791cd7887a8b5a00
vikasthakurr <vikaskumar20012001@gmail.com>
2025-05-15 16:04:08 +0530

    shape generator and form validation and github username finder

---
+++ javascript/literals.js
@@ -0,0 +1,16 @@
+let num1 = 10;
+let num2 = 30;
+
+let res = num1+num2;
+console.log(`the sum of ${num1} and ${num2} is ${res}`);
+// console.log(`the sum of ${num1} and num2 is res`);
+console.log(`my name is 
+    
+    
+    
+    
+    vikas kumar
+    
+    
+    
+    i am a trainer`)
