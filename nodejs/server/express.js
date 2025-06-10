25558715370b00b7271218ec7857200e356933a9
vikasthakurr <vikaskumar20012001@gmail.com>
2025-06-10 23:49:11 +0530

    server using express

--- nodejs/server/express.js
+++ nodejs/server/express.js
@@ -2,14 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("me hu tumara kill switch");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("me hu tumara kill switch -2");
-  next()
-});
 app.get("/", (req, res) => {
   res.end("hello from home page");
 });
@@ -18,9 +10,9 @@ app.get("/about", (req, res) => {
   res.end("hello from about page");
 });
 
-// app.get("/contact", (req, res) => {
-//   res.end("hello from contact page");
-// });
+app.get("/contact", (req, res) => {
+  res.end("hello from contact page");
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
